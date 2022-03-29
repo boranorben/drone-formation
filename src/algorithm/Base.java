1029dfc490b78c1cded93f33fb1047c85c5d2315
boranorben <zhotkp@gmail.com>
2022-03-29 16:15:43 +0900

    :art: implement discrete events

--- src/algorithm/Base.java
+++ src/algorithm/Base.java
@@ -1,72 +1,67 @@
 package algorithm;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import application.MainSceneController;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Base {
-	private MainSceneController controller;
 	private ArrayList<Drone> drones;
+	private Queue<Drone> queue;
+	private boolean isBusy;
+	private double availableTime;
 
-	public Base(MainSceneController controller) {
-		this.controller = controller;
+	public Base() {
 		this.drones = new ArrayList<Drone>();
-
-//		this.calculateDest();
+		this.queue = new LinkedList<Drone>();
+		this.isBusy = false;
+		this.availableTime = 0;
 	}
 
-	public void addDrone(int id, double degree, int velocity, int duration) {
-		List<Direction> dValue = Arrays.asList(Direction.values());
-		Direction direction = null;
-		for (Direction d : dValue) {
-			if (d.degree == degree) {
-				direction = d;
-				break;
-			}
-		}
-		this.drones.add(new Drone(id, direction, velocity, duration));
+	public void addDrone(Drone drone) {
+		this.drones.add(drone);
 	}
 
 	public ArrayList<Drone> getDrones() {
 		return this.drones;
 	}
 
-	public String toString() {
-		return String.format("The number of drones is %d", this.drones.size());
+	public boolean isBusy() {
+		return this.isBusy;
+	}
+
+	public void addQueue(Drone drone) {
+		this.queue.add(drone);
+	}
+
+	public Drone popQueue() {
+		return this.queue.poll();
+	}
+
+	public void setTime(double time) {
+		this.availableTime = time;
+	}
+
+	public double getAvailableTime() {
+		return this.availableTime;
+	}
+
+	public boolean canMove(Drone drone) {
+		if (drone.getAddTime() >= this.availableTime) {
+			this.isBusy = true;
+			return true;
+		}
+		return false;
 	}
 
 	public void moveDrones() {
-//		for (Drone drone : this.drones) {
-//			drone.move();
-//			this.controller.updateDronesPos();
-//		}
+		for (Drone drone : this.drones) {
+			drone.move();
+//			this.uiController.updateDronesPos();
+		}
 	}
 
-//	private void calculateDest() {
-//		int row = (int) Math.round(Math.sqrt(this.drones.size()));
-//		int col = 0;
-//		final int droneSize = 25;
-//
-//		if (this.drones.size() % row == 0) {
-//			col = this.drones.size() / row;
-//		} else {
-//			col = (this.drones.size() / row) + 1;
-//		}
-//
-//		// y = height = row / x = width = column
-//		int heightDist = (this.areaHeight - droneSize) / (row + 1);
-//		int widthDist = (this.areaWidth - droneSize) / (col + 1);
-//
-//		for (Drone drone : this.drones) {
-//			drone.setDestX(widthDist * ((drone.getId() % col) + 1));
-//			drone.setDestY(heightDist * ((drone.getId() / row) + 1));
-////			drone.defineDirection();
-//		}
-//	}
-
-//	private void errorFunc() {
-//	}
+	public String toString() {
+		return String.format("The number of drones is %d", this.drones.size());
+	}
 
 }
--- src/algorithm/Drone.java
+++ src/algorithm/Drone.java
@@ -5,28 +5,26 @@ public class Drone {
 	private Direction direction;
 	private int velocity;
 	private int duration;
+	
 	private double dx;
 	private double dy;
 
-	public Drone(int id, Direction direction, int velocity, int duration) {
+	private double addTime;
+
+	public Drone(int id, Direction direction, int velocity, int duration, double addTime) {
 		this.id = id;
 		this.direction = direction;
 		this.velocity = velocity;
 		this.duration = duration;
+		
 		this.dx = 0.0;
 		this.dy = 0.0;
+
+		this.addTime = addTime;
 	}
 
 	public int getId() {
 		return this.id;
-	}
-
-	public double getX() {
-		return this.dx;
-	}
-
-	public double getY() {
-		return this.dy;
 	}
 
 	public Direction getDirection() {
@@ -40,14 +38,26 @@ public class Drone {
 	public int getDuration() {
 		return this.duration;
 	}
+	
+	public double getX() {
+		return this.dx;
+	}
+
+	public double getY() {
+		return this.dy;
+	}
+
+	public double getAddTime() {
+		return this.addTime;
+	}
 
 	public void move() {
-//			this.dx += this.direction.offsetX;
-//			this.dy += this.direction.offsetY;
+		this.dx += (this.direction.offsetX * this.velocity);
+		this.dy += (this.direction.offsetY * this.velocity);
 	}
 
 	public String toString() {
-		return String.format("Drone %d at position (%.0f, %.0f), direction: %s", this.id, this.dx, this.dy,
-				this.direction);
+		return String.format("Drone %d (time added: %.0f) at position (%.0f, %.0f), direction: %s", this.id,
+				this.addTime, this.dx, this.dy, this.direction);
 	}
 }
